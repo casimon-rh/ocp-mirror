fd98b86d3e9f3db188ff980805e5ddddd23e2473
Carlos Eduardo Simón Zepeda <casimon@redhat.com>
2023-09-21 20:32:52 -0600

    Update 0-merge-secrets.sh

--- 0-merge-secrets.sh
+++ 0-merge-secrets.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cat secrets/pull-secret.txt | jq ".auths += `cat secrets/reg-secret.json`" | tr -d '[:space:]' > secrets/pull-secret-merged.json
-cp secrets/pull-secret-merged.json ~/.docker/config.json
+# Default podman location
+mkdir -p ~/.config/containers
+cp secrets/pull-secret-merged.json ~/.config/containers/auth
